318087786126ab87ae21466b9696bc9d61fb6285
5chang2 <och8808@gmail.com>
2025-12-03 10:33:34 +0900

    commentRequest 리펙토링

--- src/main/java/com/example/instagram/dto/request/CommentRequest.java
+++ src/main/java/com/example/instagram/dto/request/CommentRequest.java
@@ -8,7 +8,7 @@ import lombok.Setter;
 
 @Getter
 @Setter
-public class CommentCreateRequest {
+public class CommentRequest {
 
     @NotBlank(message = "댓글을 입력해주세요")
     @Size(max = 500, message = "댓글은 500자 이내로 작성해주세요")
