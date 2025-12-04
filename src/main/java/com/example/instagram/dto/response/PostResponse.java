b9ba6b00a43ac9136b2e3cb1b309ffec6426f0bf
5chang2 <och8808@gmail.com>
2025-12-04 14:03:10 +0900

    이미지 업로드 기능 구현

--- src/main/java/com/example/instagram/dto/response/PostResponse.java
+++ src/main/java/com/example/instagram/dto/response/PostResponse.java
@@ -14,6 +14,7 @@ public class PostResponse {
     private Long id;
     private String content;
     private LocalDateTime createdAt;
+    private String imageUrl;
 
     private Long userId;
     private String username;
@@ -27,6 +28,7 @@ public class PostResponse {
                 .id(post.getId())
                 .content(post.getContent())
                 .createdAt(post.getCreatedAt())
+                .imageUrl(post.getImageUrl())
                 .userId(post.getUser().getId())
                 .username(post.getUser().getUsername())
                 .likeCount(0)
@@ -40,6 +42,7 @@ public class PostResponse {
                 .id(post.getId())
                 .content(post.getContent())
                 .createdAt(post.getCreatedAt())
+                .imageUrl(post.getImageUrl())
                 .userId(post.getUser().getId())
                 .username(post.getUser().getUsername())
                 .likeCount(likeCount)
